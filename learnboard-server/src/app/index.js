f802eb383d4f94761bd41c053c55ac81727f0bb6
ArielFabian <gkraken333@gmail.com>
2024-08-31 11:26:08 -0600

    Commit antes de mover el script de pyhton a linux

--- learnboard-server/src/app/index.js
+++ learnboard-server/src/app/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const colabRoutes = require('./routes/colabSpacesRoutes');
+const modelRoutes = require('./routes/modelRoutes');
 
 app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/colabs', colabRoutes);
+app.use('/model', modelRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
