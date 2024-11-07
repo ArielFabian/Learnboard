118f2fcb865766f30ddcf13ee9db7cc102f0b065
HectorGonzalez <hectorjosuegc@gmail.com>
2024-11-07 12:35:33 -0600

    cambios en el overlay

--- learnboard-app/src/components/VideoSDK/Overlay.tsx
+++ learnboard-app/src/components/VideoSDK/Overlay.tsx
@@ -143,11 +143,7 @@ function Controls() {
     </div>
   );
 }
-interface MeetingViewProps {
-  onMeetingLeave: () => void;
-}
-
-function MeetingView(props: MeetingViewProps) {
+function MeetingView(props:any) {
   const [joined, setJoined] = useState<null | "JOINING" | "JOINED">(null);
   //Get the method which will be used to join the meeting.
   //We will also get the participants list to display all participants
@@ -172,13 +168,15 @@ function MeetingView(props: MeetingViewProps) {
       <div className={styles.buttonRow}>
         <Controls />
       </div>
-       <div className={styles.textRow}>
-        {Array.from(participants.keys()).map((participantId) => (
-          <ParticipantView
-            participantId={participantId}
-            key={participantId}
-          />
-        ))}
+      <div className={styles.textRow}>
+        <div className={styles.textRow}>
+          {Array.from(participants.keys()).map((participantId) => (
+            <ParticipantView
+              participantId={participantId}
+              key={participantId}
+            />
+          ))}
+        </div>
       </div>
     </div>
   ) : joined && joined == "JOINING" ? (
@@ -192,15 +190,13 @@ function MeetingView(props: MeetingViewProps) {
   );
 }
 
-const App = ({ meetingId }:{meetingId:string}) => {
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0MGVjNzBmYS0zOThkLTRkNTAtYmM5ZC00MmI3NzM3YTMyOTMiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczMDE1OTQzNSwiZXhwIjoxNzM3OTM1NDM1fQ.QNF_OU-u0VLf_-K3xno5uS1yRwc7M4UiWP5kE_A1B68";
+// Componente principal
+const App = ({meetingId}:{meetingId:any}) => {
+  // const [meetingId, setMeetingId] = useState(null);
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0MGVjNzBmYS0zOThkLTRkNTAtYmM5ZC00MmI3NzM3YTMyOTMiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczMDE1OTQzNSwiZXhwIjoxNzM3OTM1NDM1fQ.QNF_OU-u0VLf_-K3xno5uS1yRwc7M4UiWP5kE_A1B68";;
   const [userName, setUserName] = useState("");
   const [isLeaving, setIsLeaving] = useState(false);
   const [hasUsername, setHasUsername] = useState(false);
-
-  useEffect(() => {
-    console.log("meetingId recibido:", meetingId);
-  }, [meetingId]);
 
   const onMeetingLeave = () => {
     setIsLeaving(true);
@@ -213,24 +209,24 @@ const App = ({ meetingId }:{meetingId:string}) => {
 
   return !isLeaving && hasUsername ? (
     <MeetingProvider
-      config={{
-        meetingId,
-        micEnabled: true,
-        webcamEnabled: true,
-        name: userName,
-        debugMode: true,
-      }}
-      token={token}
-    >
-      <div className={styles.videocallContainer}>
-        <MeetingView
-          meetingId={meetingId}
-          onMeetingLeave={onMeetingLeave}
-          useMeeting={useMeeting}
-          useParticipant={useParticipant}
-        />
-      </div>
-    </MeetingProvider>
+        config={{
+          meetingId,
+          micEnabled: true,
+          webcamEnabled: true,
+          name: userName,
+          debugMode: true,
+        }}
+        token={token}
+      >
+        <div className={styles.videocallContainer}>
+          <MeetingView
+            meetingId={meetingId}
+            onMeetingLeave={onMeetingLeave}
+            useMeeting={useMeeting}
+            useParticipant={useParticipant}
+          />
+        </div>
+      </MeetingProvider>
   ) : token && meetingId && isLeaving ? (
     <div className={styles.centeredContainer}>
       <h1 className={styles.centeredText}>Gracias por asistir a la reunión. ¡Hasta luego!</h1>
